280302c267003c5941e75384cb783bef46bf517c
openthe88 <383155078@qq.com>
2024-10-17 10:29:35 +0800

    修复BUG

--- api/creative/advanced_program_list.go
+++ api/creative/advanced_program_list.go
@@ -10,7 +10,7 @@ import (
 // AdvancedProgramList 获取程序化创意2.0信息
 func AdvancedProgramList(ctx context.Context, clt *core.SDKClient, accessToken string, req *creative.AdvancedProgramListRequest) (*creative.AdvancedProgramListResponse, error) {
 	var resp creative.AdvancedProgramListResponse
-	err := clt.Post(ctx, accessToken, req, &resp)
+	err := clt.Post(accessToken, req, &resp)
 	if err != nil {
 		return nil, err
 	}
