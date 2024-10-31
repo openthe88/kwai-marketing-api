e494a0216b6483335feefd4a147b893d8e3003fa
杜英杰 <duyingjie@gzfei.com>
2024-10-31 15:02:42 +0800

    提交接口返回数据类型

--- api/dsp/creative/element_review_details.go
+++ api/dsp/creative/element_review_details.go
@@ -7,10 +7,10 @@ import (
 )
 
 // ElementReviewDetails 获取创意审核详情
-func ElementReviewDetails(ctx context.Context, clt *core.SDKClient, accessToken string, req *creative.ElementReviewDetailsRequest) (*[]creative.ElementReviewDetailsResponse, error) {
+func ElementReviewDetails(ctx context.Context, clt *core.SDKClient, accessToken string, req *creative.ElementReviewDetailsRequest) ([]creative.ElementReviewDetailsResponse, error) {
 	var resp []creative.ElementReviewDetailsResponse
 	if err := clt.Post(ctx, accessToken, req, &resp); err != nil {
 		return nil, err
 	}
-	return &resp, nil
+	return resp, nil
 }
